df18fc294ea9f03a966262f4e7fc81519eba7146
TheOnlyBeardedBeast <55414686+TheOnlyBeardedBeast@users.noreply.github.com>
2020-10-19 09:39:10 +0200

    Create EmailSender.cs

---
+++ C-Sharp/EmailSender.cs
@@ -0,0 +1,56 @@
+public class EmailSenderOptions
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Host { get; set; }
+        public int Port { get; set; }
+    }
+
+public interface IEmailSender
+    {
+        Task SendEmailAsync(string email, string subject, string message, bool isHtml = false);
+        Task SendEmailSAsync(string[] emails, string subject, string message, bool isHtml = false);
+    }
+
+public class EmailSender : IEmailSender
+    {
+        private readonly EmailSenderOptions options;
+        private readonly SmtpClient client;
+
+        public EmailSender(EmailSenderOptions emailSenderOptions)
+        {
+            this.options = emailSenderOptions ?? throw new ArgumentNullException("configuration");
+
+            client = new SmtpClient(this.options.Host, this.options.Port);
+            client.UseDefaultCredentials = false;
+            client.Credentials = new NetworkCredential(this.options.Email, this.options.Password);
+        }
+
+        public async Task SendEmailAsync(string email, string subject, string message, bool isHtml = false)
+        {
+            MailMessage mailmessage = new MailMessage(options.Email, email);
+
+            mailmessage.Body = message;
+            mailmessage.IsBodyHtml = isHtml;
+            mailmessage.Subject = subject;
+
+            await client.SendMailAsync(mailmessage);
+        }
+
+        public async Task SendEmailSAsync(string[] emails, string subject, string message, bool isHtml = false)
+        {
+            MailMessage mailmessage = new MailMessage();
+
+            mailmessage.From = new MailAddress(options.Email);
+            mailmessage.IsBodyHtml = isHtml;
+            mailmessage.Body = message;
+            mailmessage.Subject = subject;
+
+            foreach(string email in emails)
+            {
+                mailmessage.To.Add(email);
+            }
+
+            await client.SendMailAsync(mailmessage);
+        }
+    }
