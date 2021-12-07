f26bbe26e66eb0379ca11894f36830e7a211beec
TheOnlyBeardedBeast <55414686+TheOnlyBeardedBeast@users.noreply.github.com>
2021-12-07 22:11:59 +0100

    Added token generation helpers

---
+++ C-Sharp/Helpers.cs
@@ -0,0 +1,28 @@
+public static class Helpers
+{
+    public static string GenerateUniqueToken()
+    {
+        return Convert.ToBase64String(Guid.NewGuid().ToByteArray());
+    }
+
+    public static string GenerateTimestampedToken()
+    {
+        byte[] time = BitConverter.GetBytes(DateTime.UtcNow.ToBinary());
+        byte[] key = Guid.NewGuid().ToByteArray();
+
+        return Convert.ToBase64String(time.Concat(key).ToArray());
+    }
+
+    public static bool ValidateExpiringToken(string token, int tokenLifetime)
+    {
+        byte[] data = Convert.FromBase64String(token);
+        DateTime timestamp = DateTime.FromBinary(BitConverter.ToInt64(data, 0));
+
+        if (timestamp.AddMinutes(tokenLifetime) < DateTime.UtcNow)
+        {
+            return false;
+        }
+
+        return true;
+    }
+}
