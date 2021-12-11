55d5d2b5d242f375e8202423540f48812b5e21c4
TheOnlyBeardedBeast <55414686+TheOnlyBeardedBeast@users.noreply.github.com>
2021-12-11 13:59:49 +0100

    Added unix epoch time extension

--- C-Sharp/Extensions.cs
+++ C-Sharp/Extensions.cs
@@ -37,3 +37,11 @@
                 dbSet.Add(data);
             }
         }
+        
+        public static double GetUnixEpoch(this DateTime dateTime)
+        {
+            var unixTime = dateTime.ToUniversalTime() -
+                new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+
+            return unixTime.TotalSeconds;
+        }
