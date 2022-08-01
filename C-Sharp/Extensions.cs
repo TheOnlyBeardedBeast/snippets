437f89d13f0a337e82051fe36848087d00e4f274
TheOnlyBeardedBeast <55414686+TheOnlyBeardedBeast@users.noreply.github.com>
2022-08-01 15:29:21 +0200

    Update Extensions.cs

--- C-Sharp/Extensions.cs
+++ C-Sharp/Extensions.cs
@@ -45,3 +45,14 @@
 
             return unixTime.TotalSeconds;
         }
+
+        public static async Task DownloadFileAsync(this HttpClient client, Uri uri, string FileName)
+        {
+            using (var s = await client.GetStreamAsync(uri))
+            {
+                using (var fs = new FileStream(FileName, FileMode.CreateNew))
+                {
+                    await s.CopyToAsync(fs);
+                }
+            }
+        }
